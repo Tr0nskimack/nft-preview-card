eb6d640a0fe704bfad558ab938947038164d9480
unknown <jmarcanoy@gmail.com>
2024-03-02 10:28:32 -0400

    modificando dimensiones

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,17 @@ const App = () => {
     <div className="bg-[#0C182B] flex justify-center items-center h-screen">
       {/* card */}
 
-      <div className="bg-[#14243D] w-[370px] h-[600px] shadow-2xl rounded-2xl">
-        <header className=" overflow-hidden">
+      <div className="bg-[#14243D] w-[350px] h-[595px] shadow-2xl rounded-2xl">
+        <header >
           <img
             src="image-equilibrium.jpg"
-            className="mx-auto w-[320px] mt-6 rounded-2xl "
+            className="mx-auto w-[300px] mt-6 rounded-md "
             alt="imagen del card"
           />
         </header>
         <div className="px-8">
           <h1 className="py-4 font-semibold text-white text-2xl">
-            Equilimrium #3429
+            Equilibrium #3429
           </h1>
           <p className="text-[#8BABD9] font-light text-[18px]">
             Our Equilibrium collection promotes balance and calm.
@@ -28,14 +28,14 @@ const App = () => {
               <ImDiamonds/>
               <p className="text-[#00fcfcff]  tracking-widest">0.041ETH</p>
             </div>
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-2 mt-2">
               <span >
                 <RiTimer2Fill className="text-[#8BABD9] "/>
               </span>
               <p className="text-[#8BABD9]">3 days left</p>
             </div>
           </div>
-          <hr className="w-[310px] border-[#223754] border-[1px]  " />
+          <hr className="w-[310px] border-[#223754] border-[1px] mt-2  " />
           <div className="flex items-center gap-4 pt-4">
             <img
               src="image-avatar.png"
